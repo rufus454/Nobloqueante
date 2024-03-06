de0ae0d577cacd144ffaebf7760ec2d8b0f74bc3
rufus454 <115057265+rufus454@users.noreply.github.com>
2024-03-06 09:41:09 -0600

    ProgramaNobloqeante
    CodigoNobloqueante

---
+++ Nobloqueante.script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+setTimeout(function() {
+    console.log('Tarea 1');  
+},3000);
+
+setTimeout(function() {
+    console.log('Tarea 2');  
+},20000);
+
+setTimeout(function() {
+    console.log('Tarea 3');  
+},10000);
+
+setTimeout(function() {
+    console.log('Tarea 4');  
+},2000);
+
+console.log('Tarea 5');
+console.log(process.uptime())
+console.log('Fin del Programa');
